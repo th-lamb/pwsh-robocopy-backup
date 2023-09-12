2988f243554146540324f5359f051e3a2721d465
tholam <thomas.lambeck@ausy-technologies.de>
2023-09-12 14:09:25 +0200

    Added: FolderExists.Tests.ps1, FileExists.Tests.ps1

---
+++ Pester/filesystem-functions/FolderExists.Tests.ps1
@@ -0,0 +1,39 @@
+BeforeAll {
+  . "${PSScriptRoot}/../../lib/filesystem-functions.ps1"
+}
+
+
+
+Describe 'FolderExists' {
+  It 'Returns $true for an existing directory' {
+    $path_spec  = "${PSScriptRoot}\test_files\"
+    $expected   = $true
+
+    $result = FolderExists "${path_spec}"
+    $result | Should -be $expected
+  }
+
+  It 'Returns $false for a non-existent directory' {
+    $path_spec  = "${PSScriptRoot}\test_files2\"
+    $expected   = $false
+
+    $result = FolderExists "${path_spec}"
+    $result | Should -be $expected
+  }
+
+  It 'Returns $false for an existing file' {
+    $path_spec  = "${PSScriptRoot}\test_files\Test1\test.ini"
+    $expected   = $false
+
+    $result = FolderExists "${path_spec}"
+    $result | Should -be $expected
+  }
+
+  It 'Returns $false for a non-existent file' {
+    $path_spec  = "${PSScriptRoot}\test_files\Test1\test.xml"
+    $expected   = $false
+
+    $result = FolderExists "${path_spec}"
+    $result | Should -be $expected
+  }
+}
