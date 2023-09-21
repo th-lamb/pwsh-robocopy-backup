82c4e7f9e0c7bd031f75f007779d8f2301473bd1
tholam <thomas.lambeck@ausy-technologies.de>
2023-09-21 19:44:20 +0200

    LogAndShowMessage.Tests.ps1 complete

--- Pester/lib/logging-functions/LogAndShowMessage.Tests.ps1
+++ Pester/lib/logging-functions/LogAndShowMessage.Tests.ps1
@@ -33,11 +33,7 @@ BeforeAll {
 
 
 Describe 'LogAndShowMessage' {
-  # Check file content: https://pester.dev/docs/v4/usage/assertions#filecontentmatch
-
-  #TODO: Calls Add-LogMessage with correct message?
-  #TODO: Calls correct Write-*Message function?
-  Context 'Add-LogMessage' {
+  Context 'Step 1: Add-LogMessage' {
     It 'Calls Add-LogMessage with correct severity.' {
       # Preparation
       Mock Add-LogMessage {} -Verifiable
@@ -54,20 +50,72 @@ Describe 'LogAndShowMessage' {
       # Test(s)
       foreach( $test_severity in [SeverityKeyword].GetEnumNames() )
       {
-        #Write-Host $test_severity -ForegroundColor Yellow
         LogAndShowMessage "${logfile}" $test_severity "Test message"
 
         Should -Invoke -CommandName "Add-LogMessage" -Times 1 -Exactly -ParameterFilter {
           $severity -eq "${test_severity}"
         }
       }
-
-      # Cleanup
-      Remove-Item "${logfile}" -ErrorAction SilentlyContinue
     }
 
+    It 'Calls Add-LogMessage with correct message.' {
+      # Preparation
+      Mock Add-LogMessage {} -Verifiable
+
+      Mock Write-EmergMsg {}
+      Mock Write-AlertMsg {}
+      Mock Write-CritMsg {}
+      Mock Write-ErrMsg {}
+      Mock Write-WarningMsg {}
+      Mock Write-NoticeMsg {}
+      Mock Write-InfoMsg {}
+      Mock Write-DebugMsg {}
+
+      # Test(s)
+      foreach( $test_severity in [SeverityKeyword].GetEnumNames() )
+      {
+        $test_message = "${test_severity} message"
+        LogAndShowMessage "${logfile}" $test_severity "${test_message}"
+
+        Should -Invoke -CommandName "Add-LogMessage" -Times 1 -Exactly -ParameterFilter {
+          $message -eq "${test_message}"
+        }
+      }
+    }
   }
 
+  Context 'Step 2: Write-...Message functions' {
+    It 'Calls correct Write-...Message function with specified message' {
+      # Preparation
+      Mock Add-LogMessage {}
+
+      Mock Write-EmergMsg {} -Verifiable
+      Mock Write-AlertMsg {} -Verifiable
+      Mock Write-CritMsg {} -Verifiable
+      Mock Write-ErrMsg {} -Verifiable
+      Mock Write-WarningMsg {} -Verifiable
+      Mock Write-NoticeMsg {} -Verifiable
+      Mock Write-InfoMsg {} -Verifiable
+      Mock Write-DebugMsg {} -Verifiable
+
+      # Test(s)
+      foreach( $test_severity in [SeverityKeyword].GetEnumNames() )
+      {
+        $sb = [System.Text.StringBuilder]::new()
+        [void]$sb.Append("Write-")
+        [void]$sb.Append( $test_severity.Substring(0,1) )
+        [void]$sb.Append( $test_severity.Substring(1).ToLowerInvariant() )
+        [void]$sb.Append("Msg")
+        $command_name = $sb.ToString()
+
+        LogAndShowMessage "${logfile}" $test_severity "Test message"
+
+        Should -Invoke -CommandName "${command_name}" -Times 1 -Exactly -ParameterFilter {
+          $message -eq "Test message"
+        }
+      }
+    }
+  }
 }
 
 
