4dcf6622d5c32e8d64bb22fb07935d78de7a8c61
tholam <thomas.lambeck@ausy-technologies.de>
2023-09-24 17:06:32 +0200

    Refactored Write-ColoredMessage.Tests

--- Pester/lib/message-functions/Write-ColoredMessage.Tests.ps1
+++ Pester/lib/message-functions/Write-ColoredMessage.Tests.ps1
@@ -14,32 +14,45 @@ BeforeAll {
   . "${ProjectRoot}lib/message-functions.ps1"
   $Script:__VERBOSE = 6
 
-  function Test-SeverityWithExpectedColors {
+  function Test-SeverityExpectedBackgroundColor {
     param (
       [Parameter()]
       [SeverityKeyword]$test_severity,
       [String]$message,
-      [String]$expected_backgroundColor,
-      [String]$expected_foregroundColor
+      [String]$expected_backgroundColor
     )
 
+    #TODO: Does not fail with multiple parameter checks (e.g. $BackgroundColor + $ForegroundColor) at the same time!
     #Mock Write-Host {} -Verifiable
-    Mock Write-Host {
-      $Script:used_backgroundColor = "${BackgroundColor}"
-      $Script:used_foregroundColor = "${ForegroundColor}"
-    } -Verifiable
+    #
+    #Write-ColoredMessage $test_severity "${message}"
+    #
+    #TODO: The error message just is "was called 0 times" - but not why!
+    #Should -Invoke -CommandName "Write-Host" -Times 1 -Exactly -ParameterFilter {
+    #  $BackgroundColor -eq "${expected_backgroundColor}"
+    #}
+
+    # We store the used BackgroundColor for a better error message.
+    Mock Write-Host { $Script:used_backgroundColor = $BackgroundColor } -Verifiable
 
     Write-ColoredMessage $test_severity "${message}"
 
-    Should -Invoke -CommandName "Write-Host" -Times 1 -Exactly
-
-    #TODO: Does not fail with wrong colors, but it works in "LogAndShowMessage.Tests.ps1"!
-    #Should -Invoke -CommandName "Write-Host" -ParameterFilter {
-    #  $BackgroundColor -eq "${expected_backgroundColor}"
-    #  $ForegroundColor -eq "${expected_foregroundColor}"
-    #}
-
     $used_backgroundColor | Should -Be $expected_backgroundColor
+  }
+
+  function Test-SeverityExpectedForegroundColor {
+    param (
+      [Parameter()]
+      [SeverityKeyword]$test_severity,
+      [String]$message,
+      [String]$expected_foregroundColor
+    )
+
+    # We store the used ForegroundColor for a better error message.
+    Mock Write-Host { $Script:used_foregroundColor = $ForegroundColor } -Verifiable
+
+    Write-ColoredMessage $test_severity "${message}"
+
     $used_foregroundColor | Should -Be $expected_foregroundColor
   }
 }
@@ -48,68 +61,116 @@ BeforeAll {
 
 Describe 'Write-ColoredMessage' {
   Context 'Legal severity levels' {
-    It 'Writes a message for severity: EMERG' {
+    It 'Uses correct BackgroundColor for severity: EMERG' {
       $test_severity            = [SeverityKeyword]::EMERG
       $expected_backgroundColor = "Red"
-      $expected_foregroundColor = "White"
 
-      Test-SeverityWithExpectedColors $test_severity "This message should be written." $expected_backgroundColor $expected_foregroundColor
+      Test-SeverityExpectedBackgroundColor $test_severity "This message should be written." $expected_backgroundColor
     }
 
-    It 'Writes a message for severity: ALERT' {
+    It 'Uses correct ForegroundColor for severity: EMERG' {
+      $test_severity            = [SeverityKeyword]::EMERG
+      $expected_foregroundColor = "White"
+
+      Test-SeverityExpectedForegroundColor $test_severity "This message should be written." $expected_foregroundColor
+    }
+
+    It 'Uses correct BackgroundColor for severity: ALERT' {
       $test_severity            = [SeverityKeyword]::ALERT
       $expected_backgroundColor = "Red"
-      $expected_foregroundColor = "Black"
 
-      Test-SeverityWithExpectedColors $test_severity "This message should be written." $expected_backgroundColor $expected_foregroundColor
+      Test-SeverityExpectedBackgroundColor $test_severity "This message should be written." $expected_backgroundColor
     }
 
-    It 'Writes a message for severity: CRIT' {
+    It 'Uses correct ForegroundColor for severity: ALERT' {
+      $test_severity            = [SeverityKeyword]::ALERT
+      $expected_foregroundColor = "Black"
+
+      Test-SeverityExpectedForegroundColor $test_severity "This message should be written." $expected_foregroundColor
+    }
+
+    It 'Uses correct BackgroundColor for severity: CRIT' {
       $test_severity            = [SeverityKeyword]::CRIT
       $expected_backgroundColor = "Yellow"
-      $expected_foregroundColor = "Black"
 
-      Test-SeverityWithExpectedColors $test_severity "This message should be written." $expected_backgroundColor $expected_foregroundColor
+      Test-SeverityExpectedBackgroundColor $test_severity "This message should be written." $expected_backgroundColor
     }
 
-    It 'Writes a message for severity: ERR' {
+    It 'Uses correct ForegroundColor for severity: CRIT' {
+      $test_severity            = [SeverityKeyword]::CRIT
+      $expected_foregroundColor = "Black"
+
+      Test-SeverityExpectedForegroundColor $test_severity "This message should be written." $expected_foregroundColor
+    }
+
+    It 'Uses correct BackgroundColor for severity: ERR' {
       $test_severity            = [SeverityKeyword]::ERR
       $expected_backgroundColor = "White"
+
+      Test-SeverityExpectedBackgroundColor $test_severity "This message should be written." $expected_backgroundColor
+    }
+
+    It 'Uses correct ForegroundColor for severity: ERR' {
+      $test_severity            = [SeverityKeyword]::ERR
       $expected_foregroundColor = "Red"
 
-      Test-SeverityWithExpectedColors $test_severity "This message should be written." $expected_backgroundColor $expected_foregroundColor
+      Test-SeverityExpectedForegroundColor $test_severity "This message should be written." $expected_foregroundColor
     }
 
-    It 'Writes a message for severity: WARNING' {
+    It 'Uses correct BackgroundColor for severity: WARNING' {
       $test_severity            = [SeverityKeyword]::WARNING
       $expected_backgroundColor = "White"
+
+      Test-SeverityExpectedBackgroundColor $test_severity "This message should be written." $expected_backgroundColor
+    }
+
+    It 'Uses correct ForegroundColor for severity: WARNING' {
+      $test_severity            = [SeverityKeyword]::WARNING
       $expected_foregroundColor = "Black"
 
-      Test-SeverityWithExpectedColors $test_severity "This message should be written." $expected_backgroundColor $expected_foregroundColor
+      Test-SeverityExpectedForegroundColor $test_severity "This message should be written." $expected_foregroundColor
     }
 
-    It 'Writes a message for severity: NOTICE' {
+    It 'Uses correct BackgroundColor for severity: NOTICE' {
       $test_severity            = [SeverityKeyword]::NOTICE
       $expected_backgroundColor = "Blue"
-      $expected_foregroundColor = "White"
 
-      Test-SeverityWithExpectedColors $test_severity "This message should be written." $expected_backgroundColor $expected_foregroundColor
+      Test-SeverityExpectedBackgroundColor $test_severity "This message should be written." $expected_backgroundColor
     }
 
-    It 'Writes a message for severity: INFO' {
+    It 'Uses correct ForegroundColor for severity: NOTICE' {
+      $test_severity            = [SeverityKeyword]::NOTICE
+      $expected_foregroundColor = "White"
+
+      Test-SeverityExpectedForegroundColor $test_severity "This message should be written." $expected_foregroundColor
+    }
+
+    It 'Uses correct BackgroundColor for severity: INFO' {
       $test_severity            = [SeverityKeyword]::INFO
       $expected_backgroundColor = "Black"
-      $expected_foregroundColor = "White"
 
-      Test-SeverityWithExpectedColors $test_severity "This message should be written." $expected_backgroundColor $expected_foregroundColor
+      Test-SeverityExpectedBackgroundColor $test_severity "This message should be written." $expected_backgroundColor
     }
 
-    It 'Writes a message for severity: DEBUG' {
+    It 'Uses correct ForegroundColor for severity: INFO' {
+      $test_severity            = [SeverityKeyword]::INFO
+      $expected_foregroundColor = "White"
+
+      Test-SeverityExpectedForegroundColor $test_severity "This message should be written." $expected_foregroundColor
+    }
+
+    It 'Uses correct BackgroundColor for severity: DEBUG' {
       $test_severity            = [SeverityKeyword]::DEBUG
       $expected_backgroundColor = "Black"
+
+      Test-SeverityExpectedBackgroundColor $test_severity "This message should be written." $expected_backgroundColor
+    }
+
+    It 'Uses correct ForegroundColor for severity: DEBUG' {
+      $test_severity            = [SeverityKeyword]::DEBUG
       $expected_foregroundColor = "DarkGray"
 
-      Test-SeverityWithExpectedColors $test_severity "This message should be written." $expected_backgroundColor $expected_foregroundColor
+      Test-SeverityExpectedForegroundColor $test_severity "This message should be written." $expected_foregroundColor
     }
   }
 
