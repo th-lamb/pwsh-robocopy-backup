584f839f7cb53b6312fc60a6aa274a0fd49a0220
tholam <thomas.lambeck@ausy-technologies.de>
2023-09-25 14:33:11 +0200

    WrapperForSeverityLevels.Tests.ps1 complete

--- Pester/lib/message-functions/WrapperForSeverityLevels.Tests.ps1
+++ Pester/lib/message-functions/WrapperForSeverityLevels.Tests.ps1
@@ -10,8 +10,6 @@ BeforeAll {
 
 
 
-#TODO: Too many duplicated lines!
-
 Describe 'Write-EmergMsg' {
   It 'Calls Write-ColoredMessage with "emerg" and specified message.' {
     $test_severity    = "emerg"
@@ -68,14 +66,7 @@ Describe 'Write-ErrMsg' {
   }
 }
 
-#TODO: rest incl. verbosity check!
-
 Describe 'Write-WarningMsg' {
-#  BeforeEach {
-#    $Script:used_severity = ""
-#    $Script:used_message  = ""
-#  }
-
   It 'Calls Write-ColoredMessage with "warning" and specified message for $__VERBOSE >= 4.' {
     $test_severity    = "warning"
     $test_message     = "Test message"
@@ -103,6 +94,102 @@ Describe 'Write-WarningMsg' {
 
     {
       Write-WarningMsg -Message "Test message"
+    } | Should -Throw
+  }
+}
+
+Describe 'Write-NoticeMsg' {
+  It 'Calls Write-ColoredMessage with "notice" and specified message for $__VERBOSE >= 5.' {
+    $test_severity    = "notice"
+    $test_message     = "Test message"
+    $expected_message = "[NOTICE ] ${test_message}"
+
+    $Script:__VERBOSE = 5
+    Write-NoticeMsg -Message "${test_message}"
+
+    Should -Invoke -CommandName "Write-ColoredMessage" -Times 1 -Exactly
+    $Script:used_severity | Should -Be "${test_severity}"
+    $Script:used_message | Should -Be "${expected_message}"
+  }
+
+  It 'Does NOT call Write-ColoredMessage for $__VERBOSE < 5.' {
+    $Script:__VERBOSE = 4
+    Write-NoticeMsg -Message "Test message"
+
+    Should -Invoke -CommandName "Write-ColoredMessage" -Times 0
+  }
+
+  It 'Throws an exception if $__VERBOSE is not defined.' {
+    Mock Write-ErrMsg {}  # Omit output within the tested function.
+
+    Clear-Variable __VERBOSE -Scope Script
+
+    {
+      Write-NoticeMsg -Message "Test message"
+    } | Should -Throw
+  }
+}
+
+Describe 'Write-InfoMsg' {
+  It 'Calls Write-ColoredMessage with "info" and specified message for $__VERBOSE >= 6.' {
+    $test_severity    = "info"
+    $test_message     = "Test message"
+    $expected_message = "[INFO   ] ${test_message}"
+
+    $Script:__VERBOSE = 6
+    Write-InfoMsg -Message "${test_message}"
+
+    Should -Invoke -CommandName "Write-ColoredMessage" -Times 1 -Exactly
+    $Script:used_severity | Should -Be "${test_severity}"
+    $Script:used_message | Should -Be "${expected_message}"
+  }
+
+  It 'Does NOT call Write-ColoredMessage for $__VERBOSE < 6.' {
+    $Script:__VERBOSE = 5
+    Write-InfoMsg -Message "Test message"
+
+    Should -Invoke -CommandName "Write-ColoredMessage" -Times 0
+  }
+
+  It 'Throws an exception if $__VERBOSE is not defined.' {
+    Mock Write-ErrMsg {}  # Omit output within the tested function.
+
+    Clear-Variable __VERBOSE -Scope Script
+
+    {
+      Write-InfoMsg -Message "Test message"
+    } | Should -Throw
+  }
+}
+
+Describe 'Write-DebugMsg' {
+  It 'Calls Write-ColoredMessage with "debug" and specified message for $__VERBOSE >= 7.' {
+    $test_severity    = "debug"
+    $test_message     = "Test message"
+    $expected_message = "[DEBUG  ] ${test_message}"
+
+    $Script:__VERBOSE = 7
+    Write-DebugMsg -Message "${test_message}"
+
+    Should -Invoke -CommandName "Write-ColoredMessage" -Times 1 -Exactly
+    $Script:used_severity | Should -Be "${test_severity}"
+    $Script:used_message | Should -Be "${expected_message}"
+  }
+
+  It 'Does NOT call Write-ColoredMessage for $__VERBOSE < 7.' {
+    $Script:__VERBOSE = 6
+    Write-DebugMsg -Message "Test message"
+
+    Should -Invoke -CommandName "Write-ColoredMessage" -Times 0
+  }
+
+  It 'Throws an exception if $__VERBOSE is not defined.' {
+    Mock Write-ErrMsg {}  # Omit output within the tested function.
+
+    Clear-Variable __VERBOSE -Scope Script
+
+    {
+      Write-DebugMsg -Message "Test message"
     } | Should -Throw
   }
 }
