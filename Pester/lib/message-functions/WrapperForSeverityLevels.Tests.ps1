f488c64b87a22b1e5d60fa6c512af05ce3771a53
tholam <thomas.lambeck@ausy-technologies.de>
2023-09-25 13:44:16 +0200

    Mock Write-ColoredMessage moved to BeforeAll

--- Pester/lib/message-functions/WrapperForSeverityLevels.Tests.ps1
+++ Pester/lib/message-functions/WrapperForSeverityLevels.Tests.ps1
@@ -1,20 +1,22 @@
 BeforeAll {
   $ProjectRoot = Resolve-Path "${PSScriptRoot}/../../../"
   . "${ProjectRoot}lib/message-functions.ps1"
+
+  Mock Write-ColoredMessage {
+    $Script:used_severity = $Severity
+    $Script:used_message  = $Message
+  } -Verifiable
 }
 
 
+
+#TODO: Too many duplicated lines!
 
 Describe 'Write-EmergMsg' {
   It 'Calls Write-ColoredMessage with "emerg" and specified message.' {
     $test_severity    = "emerg"
     $test_message     = "Test message"
     $expected_message = "[EMERG  ] ${test_message}"
-
-    Mock Write-ColoredMessage {
-      $Script:used_severity = $Severity
-      $Script:used_message  = $Message
-    } -Verifiable
 
     Write-EmergMsg -Message "${test_message}"
 
@@ -29,11 +31,6 @@ Describe 'Write-AlertMsg' {
     $test_message     = "Test message"
     $expected_message = "[ALERT  ] ${test_message}"
 
-    Mock Write-ColoredMessage {
-      $Script:used_severity = $Severity
-      $Script:used_message  = $Message
-    } -Verifiable
-
     Write-AlertMsg -Message "${test_message}"
 
     $Script:used_severity | Should -Be "${test_severity}"
@@ -46,11 +43,6 @@ Describe 'Write-CritMsg' {
     $test_severity    = "crit"
     $test_message     = "Test message"
     $expected_message = "[CRIT   ] ${test_message}"
-
-    Mock Write-ColoredMessage {
-      $Script:used_severity = $Severity
-      $Script:used_message  = $Message
-    } -Verifiable
 
     Write-CritMsg -Message "${test_message}"
 
@@ -65,11 +57,6 @@ Describe 'Write-ErrMsg' {
     $test_message     = "Test message"
     $expected_message = "[ERR    ] ${test_message}"
 
-    Mock Write-ColoredMessage {
-      $Script:used_severity = $Severity
-      $Script:used_message  = $Message
-    } -Verifiable
-
     Write-ErrMsg -Message "${test_message}"
 
     $Script:used_severity | Should -Be "${test_severity}"
@@ -80,49 +67,38 @@ Describe 'Write-ErrMsg' {
 #TODO: rest incl. verbosity check!
 
 Describe 'Write-WarningMsg' {
-  It 'Calls Write-ColoredMessage with "warning" and specified message for $__VERBOSE `>= 4.' {
+#  BeforeEach {
+#    $Script:used_severity = ""
+#    $Script:used_message  = ""
+#  }
+
+  It 'Calls Write-ColoredMessage with "warning" and specified message for $__VERBOSE >= 4.' {
     $test_severity    = "warning"
     $test_message     = "Test message"
     $expected_message = "[WARNING] ${test_message}"
 
-    Mock Write-ColoredMessage {
-      $Script:used_severity = $Severity
-      $Script:used_message  = $Message
-    } -Verifiable
-
     $Script:__VERBOSE = 4
     Write-WarningMsg -Message "${test_message}"
 
+    Should -Invoke -CommandName "Write-ColoredMessage" -Times 1 -Exactly
     $Script:used_severity | Should -Be "${test_severity}"
     $Script:used_message | Should -Be "${expected_message}"
   }
 
   It 'Does NOT call Write-ColoredMessage for $__VERBOSE < 4.' {
-    $test_severity    = "warning"
-    $test_message     = "Test message"
-    $expected_message = "[WARNING] ${test_message}"
+    $Script:__VERBOSE = 3
+    Write-WarningMsg -Message "Test message"
 
-    Mock Write-ColoredMessage {
-      $Script:used_severity = $Severity
-      $Script:used_message  = $Message
-    } -Verifiable
-
-    $Script:__VERBOSE = 4
-    Write-WarningMsg -Message "${test_message}"
-
-    $Script:used_severity | Should -Be "${test_severity}"
-    $Script:used_message | Should -Be "${expected_message}"
+    Should -Invoke -CommandName "Write-ColoredMessage" -Times 0
   }
 
   It 'Throws an exception if $__VERBOSE is not defined.' {
-    $test_message     = "Test message"
-
     Mock Write-ErrMsg {}  # Omit output within the tested function.
 
     Clear-Variable __VERBOSE -Scope Script
 
     {
-      Write-WarningMsg -Message "${test_message}"
+      Write-WarningMsg -Message "Test message"
     } | Should -Throw
   }
 }
