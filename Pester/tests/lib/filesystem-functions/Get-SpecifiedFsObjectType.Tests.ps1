3f86d36564748d0448f7486b8b8518d219de7e62
tholam <thomas.lambeck@ausy-technologies.de>
2026-03-15 23:44:25 +0100

    More generic test

--- Pester/tests/lib/filesystem-functions/Get-SpecifiedFsObjectType.Tests.ps1
+++ Pester/tests/lib/filesystem-functions/Get-SpecifiedFsObjectType.Tests.ps1
@@ -9,16 +9,16 @@ BeforeAll {
 Describe 'Get-SpecifiedFsObjectType' {
   Context 'Directories/files without patterns' {
     It 'recognizes a directory                              e.g. C:\Users\...\Music\' {
-      $path_spec  = "${workingFolder}Music\"
-      $expected   = "directory"
+      $path_spec = "${workingFolder}Music\"
+      $expected = "directory"
 
       $object_type = Get-SpecifiedFsObjectType "${path_spec}"
       "${object_type}" | Should -Be "${expected}"
     }
 
     It 'recognizes a file                                   e.g. C:\Users\...\Music\title1.mp3' {
-      $path_spec  = "${workingFolder}Music\title1.mp3"
-      $expected   = "file"
+      $path_spec = "${workingFolder}Music\title1.mp3"
+      $expected = "file"
 
       $object_type = Get-SpecifiedFsObjectType "${path_spec}"
       "${object_type}" | Should -Be "${expected}"
@@ -27,16 +27,16 @@ Describe 'Get-SpecifiedFsObjectType' {
 
   Context 'Directories/files with patterns' {
     It 'recognizes a directory pattern                      e.g. C:\Users\...\Mu*ic\' {
-      $path_spec  = "${workingFolder}Mu*ic\"
-      $expected   = "directory pattern"
+      $path_spec = "${workingFolder}Mu*ic\"
+      $expected = "directory pattern"
 
       $object_type = Get-SpecifiedFsObjectType "${path_spec}"
       "${object_type}" | Should -Be "${expected}"
     }
 
     It 'recognizes a file pattern                           e.g. C:\Users\...\Music\title*.mp3' {
-      $path_spec  = "${workingFolder}Music\title*.mp3"
-      $expected   = "file pattern"
+      $path_spec = "${workingFolder}Music\title*.mp3"
+      $expected = "file pattern"
 
       $object_type = Get-SpecifiedFsObjectType "${path_spec}"
       "${object_type}" | Should -Be "${expected}"
@@ -45,8 +45,8 @@ Describe 'Get-SpecifiedFsObjectType' {
 
   Context 'Placeholders in both, directory and filename' {
     It 'recognizes a directory pattern                      e.g. C:\Users\...\Mu*ic\title*.mp3' {
-      $path_spec  = "${workingFolder}Mu*ic\title*.mp3"
-      $expected   = "directory pattern"
+      $path_spec = "${workingFolder}Mu*ic\title*.mp3"
+      $expected = "directory pattern"
 
       $object_type = Get-SpecifiedFsObjectType "${path_spec}"
       "${object_type}" | Should -Be "${expected}"
@@ -55,16 +55,16 @@ Describe 'Get-SpecifiedFsObjectType' {
 
   Context 'Drive letters' {
     It 'recognizes existing drive letter without \          e.g. C:' {
-      $path_spec  = "C:"
-      $expected   = "drive letter"
+      $path_spec = "C:"
+      $expected = "drive letter"
 
       $object_type = Get-SpecifiedFsObjectType "${path_spec}"
       "${object_type}" | Should -Be "${expected}"
     }
 
     It 'recognizes existing drive letter with \             e.g. C:\' {
-      $path_spec  = "C:\"
-      $expected   = "drive letter"
+      $path_spec = "C:\"
+      $expected = "drive letter"
 
       $object_type = Get-SpecifiedFsObjectType "${path_spec}"
       "${object_type}" | Should -Be "${expected}"
@@ -73,51 +73,50 @@ Describe 'Get-SpecifiedFsObjectType' {
 
   Context 'Network shares' {
     It 'recognizes network share with trailing backslash' {
-      $path_spec  = "\\NODE304\Backup\"
-      $expected   = "network share"
+      $path_spec = "\\Server\Backup\"
+      $expected = "network share"
 
       $object_type = Get-SpecifiedFsObjectType "${path_spec}"
       "${object_type}" | Should -Be "${expected}"
     }
 
     It 'recognizes network share without trailing backslash' {
-      $path_spec  = "\\NODE304\Backup"
-      $expected   = "network share"
+      $path_spec = "\\Server\Backup"
+      $expected = "network share"
 
       $object_type = Get-SpecifiedFsObjectType "${path_spec}"
       "${object_type}" | Should -Be "${expected}"
     }
 
     It 'recognizes subfolder of a network share' {
-      $path_spec  = "\\NODE304\Backup\win-backup\"
-      $expected   = "directory"
+      $path_spec = "\\Server\Backup\win-backup\"
+      $expected = "directory"
 
       $object_type = Get-SpecifiedFsObjectType "${path_spec}"
       "${object_type}" | Should -Be "${expected}"
     }
 
-    #TODO: File on a network share
-#    It 'recognizes file on a network share' {
-#      $path_spec  = "\\NODE304\Backup\backup-thomas-ThinkPad-T420s.sh"
-#      $expected   = "file"
-#
-#      $object_type = Get-SpecifiedFsObjectType "${path_spec}"
-#      "${object_type}" | Should -Be "${expected}"
-#    }
+    It 'recognizes file on a network share' {
+      $path_spec = "\\Server\Backup\backup-script.sh"
+      $expected = "file"
+
+      $object_type = Get-SpecifiedFsObjectType "${path_spec}"
+      "${object_type}" | Should -Be "${expected}"
+    }
   }
 
   Context 'Network computer' {
     It 'recognizes network computer with trailing backslash' {
-      $path_spec  = "\\NODE304\"
-      $expected   = "network computer"
+      $path_spec = "\\Server\"
+      $expected = "network computer"
 
       $object_type = Get-SpecifiedFsObjectType "${path_spec}"
       "${object_type}" | Should -Be "${expected}"
     }
 
     It 'recognizes network computer without trailing backslash' {
-      $path_spec  = "\\NODE304"
-      $expected   = "network computer"
+      $path_spec = "\\Server"
+      $expected = "network computer"
 
       $object_type = Get-SpecifiedFsObjectType "${path_spec}"
       "${object_type}" | Should -Be "${expected}"
@@ -126,16 +125,16 @@ Describe 'Get-SpecifiedFsObjectType' {
 
   Context '"." and ".." entries' {
     It 'recognizes "." (current folder)' {
-      $path_spec  = "C:\Users\<...>\Music\."
-      $expected   = "directory entry"
+      $path_spec = "C:\Users\<...>\Music\."
+      $expected = "directory entry"
 
       $object_type = Get-SpecifiedFsObjectType "${path_spec}"
       "${object_type}" | Should -Be "${expected}"
     }
 
     It 'recognizes ".." (parent folder)' {
-      $path_spec  = "C:\Users\<...>\Music\.."
-      $expected   = "directory entry"
+      $path_spec = "C:\Users\<...>\Music\.."
+      $expected = "directory entry"
 
       $object_type = Get-SpecifiedFsObjectType "${path_spec}"
       "${object_type}" | Should -Be "${expected}"
@@ -150,16 +149,16 @@ Describe 'Get-SpecifiedFsObjectType' {
     }
 
     It 'recognizes Known Folder without first %             e.g. UserProfile%\' {
-      $path_spec  = "UserProfile%\"
-      $expected   = "directory"
+      $path_spec = "UserProfile%\"
+      $expected = "directory"
 
       $object_type = Get-SpecifiedFsObjectType "${path_spec}"
       "${object_type}" | Should -Be "${expected}"
     }
 
     It 'recognizes files in Known Folder without first %    e.g. UserProfile%\.gitconfig' {
-      $path_spec  = "UserProfile%\.gitconfig"
-      $expected   = "file"
+      $path_spec = "UserProfile%\.gitconfig"
+      $expected = "file"
 
       $object_type = Get-SpecifiedFsObjectType "${path_spec}"
       "${object_type}" | Should -Be "${expected}"
