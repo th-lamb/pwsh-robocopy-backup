430ab3c09ea17cff7cea5483995be6ee2e183278
tholam <thomas.lambeck@ausy-technologies.de>
2026-03-16 21:20:02 +0100

    Tests extended and updated

--- Pester/tests/lib/message-functions/Write-ColoredMessage.Tests.ps1
+++ Pester/tests/lib/message-functions/Write-ColoredMessage.Tests.ps1
@@ -1,14 +1,3 @@
-enum SeverityKeyword {
-  EMERG   = 0
-  ALERT   = 1
-  CRIT    = 2
-  ERR     = 3
-  WARNING = 4
-  NOTICE  = 5
-  INFO    = 6
-  DEBUG   = 7
-}
-
 BeforeAll {
   $ProjectRoot = Resolve-Path "${PSScriptRoot}/../../../../"
   . "${ProjectRoot}lib/message-functions.ps1"
@@ -57,116 +46,150 @@ BeforeAll {
 
 
 
+# Test the Enum definition
+Describe 'SeverityKeyword Enum' {
+  It 'EMERG should be 0' {
+    [int][SeverityKeyword]::EMERG | Should -Be 0
+  }
+  It 'ALERT should be 1' {
+    [int][SeverityKeyword]::ALERT | Should -Be 1
+  }
+  It 'CRIT should be 2' {
+    [int][SeverityKeyword]::CRIT | Should -Be 2
+  }
+  It 'ERR should be 3' {
+    [int][SeverityKeyword]::ERR | Should -Be 3
+  }
+  It 'WARNING should be 4' {
+    [int][SeverityKeyword]::WARNING | Should -Be 4
+  }
+  It 'NOTICE should be 5' {
+    [int][SeverityKeyword]::NOTICE | Should -Be 5
+  }
+  It 'INFO should be 6' {
+    [int][SeverityKeyword]::INFO | Should -Be 6
+  }
+  It 'DEBUG should be 6' {
+    [int][SeverityKeyword]::DEBUG | Should -Be 7
+  }
+}
+
+
+
+<# Test Write-ColoredMessage with integer values: Function accepts 
+  integers (which PowerShell casts to the enum) and actually triggers
+  the correct behavio, for example 0 -> EMERG?
+#>
 Describe 'Write-ColoredMessage' {
   Context 'Legal severity levels' {
-    It 'Uses correct BackgroundColor for severity: EMERG' {
-      $test_severity  = [SeverityKeyword]::EMERG
-      $expected       = "Red"
+    It 'Uses correct BackgroundColor for severity 0 = EMERG' {
+      $test_severity = 0
+      $expected = "Red"
 
       Test-SeverityExpectedBackgroundColor -Severity $test_severity -Message "This message should be written." -ExpectedBackgroundColor $expected
     }
 
-    It 'Uses correct ForegroundColor for severity: EMERG' {
-      $test_severity  = [SeverityKeyword]::EMERG
-      $expected       = "White"
+    It 'Uses correct ForegroundColor for severity 0 = EMERG' {
+      $test_severity = 0
+      $expected = "White"
 
       Test-SeverityExpectedForegroundColor -Severity $test_severity -Message "This message should be written." -ExpectedForegroundColor $expected
     }
 
-    It 'Uses correct BackgroundColor for severity: ALERT' {
-      $test_severity  = [SeverityKeyword]::ALERT
-      $expected       = "Red"
+    It 'Uses correct BackgroundColor for severity 1 = ALERT' {
+      $test_severity = 1
+      $expected = "Red"
 
       Test-SeverityExpectedBackgroundColor -Severity $test_severity -Message "This message should be written." -ExpectedBackgroundColor $expected
     }
 
-    It 'Uses correct ForegroundColor for severity: ALERT' {
-      $test_severity  = [SeverityKeyword]::ALERT
-      $expected       = "Black"
+    It 'Uses correct ForegroundColor for severity 1 = ALERT' {
+      $test_severity = 1
+      $expected = "Black"
 
       Test-SeverityExpectedForegroundColor -Severity $test_severity -Message "This message should be written." -ExpectedForegroundColor $expected
     }
 
-    It 'Uses correct BackgroundColor for severity: CRIT' {
-      $test_severity  = [SeverityKeyword]::CRIT
-      $expected       = "Yellow"
+    It 'Uses correct BackgroundColor for severity 2 = CRIT' {
+      $test_severity = 2
+      $expected = "Yellow"
 
       Test-SeverityExpectedBackgroundColor -Severity $test_severity -Message "This message should be written." -ExpectedBackgroundColor $expected
     }
 
-    It 'Uses correct ForegroundColor for severity: CRIT' {
-      $test_severity  = [SeverityKeyword]::CRIT
-      $expected       = "Black"
+    It 'Uses correct ForegroundColor for severity 2 = CRIT' {
+      $test_severity = 2
+      $expected = "Black"
 
       Test-SeverityExpectedForegroundColor -Severity $test_severity -Message "This message should be written." -ExpectedForegroundColor $expected
     }
 
-    It 'Uses correct BackgroundColor for severity: ERR' {
-      $test_severity  = [SeverityKeyword]::ERR
-      $expected       = "White"
+    It 'Uses correct BackgroundColor for severity 3 = ERR' {
+      $test_severity = 3
+      $expected = "White"
 
       Test-SeverityExpectedBackgroundColor -Severity $test_severity -Message "This message should be written." -ExpectedBackgroundColor $expected
     }
 
-    It 'Uses correct ForegroundColor for severity: ERR' {
-      $test_severity  = [SeverityKeyword]::ERR
-      $expected       = "Red"
+    It 'Uses correct ForegroundColor for severity 3 = ERR' {
+      $test_severity = 3
+      $expected = "Red"
 
       Test-SeverityExpectedForegroundColor -Severity $test_severity -Message "This message should be written." -ExpectedForegroundColor $expected
     }
 
-    It 'Uses correct BackgroundColor for severity: WARNING' {
-      $test_severity  = [SeverityKeyword]::WARNING
-      $expected       = "White"
+    It 'Uses correct BackgroundColor for severity 4 = WARNING' {
+      $test_severity = 4
+      $expected = "White"
 
       Test-SeverityExpectedBackgroundColor -Severity $test_severity -Message "This message should be written." -ExpectedBackgroundColor $expected
     }
 
-    It 'Uses correct ForegroundColor for severity: WARNING' {
-      $test_severity  = [SeverityKeyword]::WARNING
-      $expected       = "Black"
+    It 'Uses correct ForegroundColor for severity 4 = WARNING' {
+      $test_severity = 4
+      $expected = "Black"
 
       Test-SeverityExpectedForegroundColor -Severity $test_severity -Message "This message should be written." -ExpectedForegroundColor $expected
     }
 
-    It 'Uses correct BackgroundColor for severity: NOTICE' {
-      $test_severity  = [SeverityKeyword]::NOTICE
-      $expected       = "Blue"
+    It 'Uses correct BackgroundColor for severity 5 = NOTICE' {
+      $test_severity = 5
+      $expected = "Blue"
 
       Test-SeverityExpectedBackgroundColor -Severity $test_severity -Message "This message should be written." -ExpectedBackgroundColor $expected
     }
 
-    It 'Uses correct ForegroundColor for severity: NOTICE' {
-      $test_severity  = [SeverityKeyword]::NOTICE
-      $expected       = "White"
+    It 'Uses correct ForegroundColor for severity 5 = NOTICE' {
+      $test_severity = 5
+      $expected = "White"
 
       Test-SeverityExpectedForegroundColor -Severity $test_severity -Message "This message should be written." -ExpectedForegroundColor $expected
     }
 
-    It 'Uses correct BackgroundColor for severity: INFO' {
-      $test_severity  = [SeverityKeyword]::INFO
-      $expected       = "Black"
+    It 'Uses correct BackgroundColor for severity 6 = INFO' {
+      $test_severity = 6
+      $expected = "Black"
 
       Test-SeverityExpectedBackgroundColor -Severity $test_severity -Message "This message should be written." -ExpectedBackgroundColor $expected
     }
 
-    It 'Uses correct ForegroundColor for severity: INFO' {
-      $test_severity  = [SeverityKeyword]::INFO
-      $expected       = "White"
+    It 'Uses correct ForegroundColor for severity 6 = INFO' {
+      $test_severity = 6
+      $expected = "White"
 
       Test-SeverityExpectedForegroundColor -Severity $test_severity -Message "This message should be written." -ExpectedForegroundColor $expected
     }
 
-    It 'Uses correct BackgroundColor for severity: DEBUG' {
-      $test_severity  = [SeverityKeyword]::DEBUG
-      $expected       = "Black"
+    It 'Uses correct BackgroundColor for severity 7 = DEBUG' {
+      $test_severity = 7
+      $expected = "Black"
 
       Test-SeverityExpectedBackgroundColor -Severity $test_severity -Message "This message should be written." -ExpectedBackgroundColor $expected
     }
 
-    It 'Uses correct ForegroundColor for severity: DEBUG' {
-      $test_severity  = [SeverityKeyword]::DEBUG
-      $expected       = "DarkGray"
+    It 'Uses correct ForegroundColor for severity 7 = DEBUG' {
+      $test_severity = 7
+      $expected = "DarkGray"
 
       Test-SeverityExpectedForegroundColor -Severity $test_severity -Message "This message should be written." -ExpectedForegroundColor $expected
     }
@@ -185,45 +208,30 @@ Describe 'Write-ColoredMessage' {
       } | Should -Throw
     }
 
-    It 'Does NOT write a message for severity: 0' {
-      $test_severity = 0
+    It 'Does NOT write a message for severity: 8' {
+      $test_severity = 8
 
       {
         Write-ColoredMessage -Severity $test_severity -Message "This message should NOT be written!"
       } | Should -Throw
     }
 
-    It 'Does NOT write a message for severity: 1' {
-      $test_severity = 1
-      $expected_message = "Write-ColoredMessage(): Illegal severity level specified: *"   # Using wildcard
-
-      {
-        Write-ColoredMessage -Severity $test_severity -Message "This message should NOT be written!"
-      } | Should -Throw -ExpectedMessage "${expected_message}"
-    }
-
-    It 'Does NOT write a message for severity: 8' {
-      $test_severity = 8
-      $expected_message = "Write-ColoredMessage(): Illegal severity level specified: *"   # Using wildcard
-
-      {
-        Write-ColoredMessage -Severity $test_severity -Message "This message should NOT be written!"
-      } | Should -Throw -ExpectedMessage "${expected_message}"
-    }
-
     It 'Does NOT write a message for severity: foo' {
       $test_severity = "foo"
-      $expected_message = "Write-ColoredMessage(): Illegal severity level specified: *"   # Using wildcard
 
       {
         Write-ColoredMessage -Severity $test_severity -Message "This message should NOT be written!"
-      } | Should -Throw -ExpectedMessage "${expected_message}"
+      } | Should -Throw
     }
   }
 
   #TODO: Redirects errors (emerg...warning) to stderr and other messages to stdout.
-
-  #TODO: Use the enum in logging-functions for $severity?
+  <#
+    Since Write-ColoredMessage currently uses Write-Host for both error and non-error
+    messages, there's no observable difference between them for a Pester test.
+    Once the stderr redirection is implemented, we'll be able to test it by checking
+    if a message was written to the error stream.
+  #>
 
   Context 'Wrong Usage' {
     It 'Throws an exception when called without severity.' {
