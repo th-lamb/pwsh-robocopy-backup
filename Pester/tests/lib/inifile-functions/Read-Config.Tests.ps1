565a2df88c3238db824fd22204af45375710b01e
tholam <thomas.lambeck@ausy-technologies.de>
2026-04-07 14:51:17 +0200

    Test added
    Normalizes directories with missing trailing backslash

--- Pester/tests/lib/inifile-functions/Read-Config.Tests.ps1
+++ Pester/tests/lib/inifile-functions/Read-Config.Tests.ps1
@@ -18,7 +18,7 @@ BeforeAll {
 
 Describe 'Read-Config' {
   Context 'Error Handling' {
-    It 'Writes a warning if the settings file is missing.' {
+    It 'Writes a warning if the INI file is missing.' {
       $IniFile = "NON_EXISTENT_FILE.ini"
       $ExpectedWarning = "Settings file not found: [$IniFile]. Using default values."
 
@@ -34,6 +34,36 @@ Describe 'Read-Config' {
       $result.GetType().Name | Should -Be "ScriptConfig"
       $result.General.__VERBOSE | Should -Be 6
     }
+
+    It 'Normalizes directories with missing trailing backslash' {
+      $ResourcesDir = "${ProjectRoot}\Pester\resources\lib\inifile-functions\Read-Config"
+      $IniFile = "${ResourcesDir}\INI-without-backslash-in-middle.ini"
+
+      $ExpectedBackupBaseDir = "C:\Backups\"
+
+      $Config = Read-Config -IniFile $IniFile
+      $Config.Normalize()
+
+      $Config.Directories.BACKUP_BASE_DIR | Should -Be "${ExpectedBackupBaseDir}"
+    }
+
+    <#TODO: Test missing backslash in the middle. Example:
+      - The user defines `BACKUP_BASE_DIR=C:\Backups` (without trailing backslash) and
+      - re-uses this variable in `BACKUP_USER_BASE_DIR=${BACKUP_BASE_DIR}%USERNAME%\`.
+      - When `$Config.Normalize()` is called, the expanded paths are already assembled and
+        the *missing "\" is in the middle* of the string!
+    #>
+    # It 'Normalizes combined paths with missing "\" in the middle' {
+    #   $ResourcesDir = "${ProjectRoot}\Pester\resources\lib\inifile-functions\Read-Config"
+    #   $IniFile = "${ResourcesDir}\INI-without-backslash-in-middle.ini"
+
+    #   $ExpectedBackupUserBaseDir = "C:\Backups\testuser\"
+
+    #   $Config = Read-Config -IniFile $IniFile
+    #   $Config.Normalize()
+
+    #   $Config.Directories.BACKUP_USER_BASE_DIR | Should -Be "${ExpectedBackupUserBaseDir}"
+    # }
   }
 
   Context 'Invalid Parameters' {
